cd6f1d644e2797e6f9bb51977dd82de03105a390
SUNUK-KIM <66252231+SUNUK-KIM@users.noreply.github.com>
2020-11-20 14:49:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const path = require('path');
-const env = process.env.ENV;
-const app = express();
-app.get('/ping', function(req, res) {
-  res.send('pong');
-});
-app.use('/', express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-  res.set({
-    "Cache-Control": "no-cache, no-store, must-revalidate",
-    "Pragma": "no-cache",
-    "Date": new Date()
-  });
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(3000, () => {
-  console.log('listening: ' + env);
-});
+/**
+ * *웹 서버에 html 파일 서비스하기
+ */
+
+var http = require("http");
+var fs = require('fs'); //파일 읽기, 쓰기 등을 할 수 있는 모듈
+
+// 404 error message: 페이지 오류가 발생했을 때,
+function send404Message(response) {
+    response.writeHead(404, { "content-Type": "text/plain" }); //글자 출력
+    response.write("404 Error...");
+    response.end();
+}
+
+// 200 Okay : 정상적인 요청
+function onRequest(request, response) {
+    if (request.method == 'GET' && request.url == '/') {
+        response.writeHead(200, { "Content-Type": "text/html" }); //웹페이지 출력
+        fs.createReadStream("./index.html").pipe(response); //같은 디렉토리에 있는 index.html를  response함
+    } else {
+        //파일이 존재하지 않을 때,
+        send404Message(response);
+    }
+}
+
+http.createServer(onRequest).listen(8080);
+console.log("Server Created...");
